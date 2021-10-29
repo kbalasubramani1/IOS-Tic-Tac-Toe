e574525eecc7ad1a07500ab81b35acf6571966dc
Kavin Bala <kbalasubramani@bsd220.org>
2021-10-29 10:43:44 -0500

    Initial Commit

---
+++ IOS Tic Tac Toe/IOS_Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Tic_Tac_ToeApp.swift
+//  IOS Tic Tac Toe
+//
+//  Created by Student on 10/29/21.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
